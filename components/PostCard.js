c7940209e1087e69cc0225b02cbcf907cfe2938e
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-17 15:04:10 -0700

    added main copy to post card

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { Box, Text, Heading, Stack } from "@chakra-ui/react";
 
-export default function MainCard({ title, img, imgAlt, meta }) {
+export default function MainCard({ title, copy, img, imgAlt, meta }) {
   return (
     <Stack direction={["column", "column", "row"]} spacing={3}>
       <Box width="full">
-        <Image src={img} alt={imgAlt} objectFit="contain" placeholder="blur" />
+        <Image src={img} alt={imgAlt} objectFit="cover" placeholder="blur" />
       </Box>
       <Box>
         <Box>
@@ -19,11 +19,7 @@ export default function MainCard({ title, img, imgAlt, meta }) {
         </Box>
 
         <Text fontFamily="Open Sans" mt={4}>
-          You know what? It is beets. I&apos;ve crashed into a beet truck.
-          Forget the fat lady! You&apos;re obsessed with the fat lady! Drive us
-          out of here! Hey, take a look at the earthlings. Goodbye! Hey, take a
-          look at the earthlings. Goodbye! Hey, take a look at the earthlings.
-          Goodbye!
+          {copy}
         </Text>
       </Box>
     </Stack>
