214b3ef5a1bfb6300ca22519530290d9a8a4ec07
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-17 14:52:18 -0700

    added icons to footer

--- layout/Footer.js
+++ layout/Footer.js
@@ -1,5 +1,21 @@
-import { Center } from "@chakra-ui/react";
-
+import { Center, HStack } from "@chakra-ui/react";
+import {
+  SiFacebook,
+  SiTwitter,
+  SiLinkedin,
+  SiInstagram,
+  SiWhatsapp,
+} from "react-icons/si";
 export default function Footer() {
-  return <Center>footer</Center>;
+  return (
+    <Center>
+      <HStack spacing={12}>
+        <SiFacebook color="#333" size="1.3em" />
+        <SiTwitter color="#333" size="1.3em" />
+        <SiLinkedin color="#333" size="1.3em" />
+        <SiInstagram color="#333" size="1.3em" />
+        <SiWhatsapp color="#333" size="1.3em" />
+      </HStack>
+    </Center>
+  );
 }
