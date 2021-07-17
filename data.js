7f16d7fc0fa6b83fdbb13c08c69fed8354040f80
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-17 14:53:18 -0700

    added blog data

--- data.js
+++ data.js
@@ -1,37 +1,44 @@
+import one from "@public/images/one.png";
+import two from "@public/images/two.png";
+import three from "@public/images/three.png";
+import four from "@public/images/four.jpg";
+
 export default function data() {
   return [
     {
-      id: "1",
+      id: 1,
       title: "How to be more productive?",
       meta: "19 May 2021, 3 comments",
-      copy: "",
-      img: "",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
+      img: one,
       imgAlt: "an img",
+      imgUrl: "@public/images/one.png",
     },
     {
-      id: "2",
+      id: 2,
       title:
         "Do these every single day and you will become a more creative person",
       meta: "19 May 2021, 3 comments",
-      copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
-      img: "",
+      copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad ...",
+      img: two,
       imgAlt: "an img",
+      imgUrl: "@public/images/two.png",
     },
     {
-      id: "3",
+      id: 3,
       title: "Destinations you want to visit",
       meta: "19 May 2021, 3 comments",
       copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
-      img: "",
-      imgAlt: "an img",
+      img: three,
+      imgAlt: "@public/images/three.png",
     },
     {
-      id: "4",
+      id: 4,
       title: "A letter to my family",
       meta: "19 May 2021, 3 comments",
       copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
-      img: "",
-      imgAlt: "an img",
+      img: four,
+      imgAlt: "@public/images/four.jpg",
     },
   ];
 }
