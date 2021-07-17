e49fc4a5fd2106cf90839587c435560a0563e7f1
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-17 15:25:44 -0700

    adjusted width and height of img box

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,11 +2,23 @@ import Image from "next/image";
 import Link from "next/link";
 import { Box, Text, Heading, Stack } from "@chakra-ui/react";
 
+/**
+ * ! use either 400px or 100% as width of the img box - for my experiments
+ *
+ *
+ */
+
 export default function MainCard({ title, copy, img, imgAlt, meta }) {
   return (
     <Stack direction={["column", "column", "row"]} spacing={3}>
-      <Box width="full">
-        <Image src={img} alt={imgAlt} objectFit="cover" placeholder="blur" />
+      <Box position="relative" width={["100%", "100%", "400px"]} height="300px">
+        <Image
+          src={img}
+          alt={imgAlt}
+          objectFit="cover"
+          placeholder="blur"
+          layout="fill"
+        />
       </Box>
       <Box>
         <Box>
