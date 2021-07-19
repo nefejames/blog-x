0ae28d3a1166e2573a0923b45edc1d192f78f0d4
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-19 15:33:44 -0700

    changed max width of layout

--- layout/Layout.js
+++ layout/Layout.js
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/layout";
 
 export default function Layout({ children }) {
   return (
-    <Box maxW="1100px" w="full" mx="auto" px={4} mt={[2, 4, 6]} mb={[2, 4, 12]}>
+    <Box maxW="1000px" w="full" mx="auto" px={4} mt={[2, 4, 6]} mb={[2, 4, 12]}>
       {children}
     </Box>
   );
