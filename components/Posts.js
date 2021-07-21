79487703d43173c2d35b0ee8822d3adbd8002d0b
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-20 17:34:57 -0700

    chore: refactored

--- components/Posts.js
+++ components/Posts.js
@@ -2,11 +2,10 @@ import { VStack } from "@chakra-ui/react";
 import PostCard from "./PostCard";
 import data from "data";
 
-const posts = data();
 export default function Posts() {
   return (
     <VStack spacing={16}>
-      {posts.map((post) => (
+      {data.map((post) => (
         <PostCard key={post.id} {...post} />
       ))}
     </VStack>
