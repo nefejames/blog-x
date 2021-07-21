8e0742bb2a49e45b675914c556411c37531d11c3
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-20 17:35:15 -0700

    made logo a link

--- layout/Header.js
+++ layout/Header.js
@@ -1,4 +1,6 @@
 import { Center, Heading } from "@chakra-ui/react";
+import Link from "next/link";
+
 export default function Header() {
   return (
     <Center
@@ -6,7 +8,9 @@ export default function Header() {
       fontSize={["xl", "2xl", "3xl"]}
       my={[8, 16]}
     >
-      <Heading as="h1">BlogX</Heading>
+      <Link href="/" passHref>
+        <Heading as="a">BlogX</Heading>
+      </Link>
     </Center>
   );
 }
