aef02cfbc17bfd55ab3365c467b1267d70db8a45
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-20 17:31:27 -0700

    changed data to array

--- data.js
+++ data.js
@@ -3,42 +3,42 @@ import two from "@public/images/two.png";
 import three from "@public/images/three.png";
 import four from "@public/images/four.jpg";
 
-export default function data() {
-  return [
-    {
-      id: 1,
-      title: "How to be more productive?",
-      meta: "19 May 2021, 3 comments",
-      copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
-      img: one,
-      imgAlt: "an img",
-      imgUrl: "@public/images/one.png",
-    },
-    {
-      id: 2,
-      title:
-        "Do these every single day and you will become a more creative person",
-      meta: "19 May 2021, 3 comments",
-      copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad ...",
-      img: two,
-      imgAlt: "an img",
-      imgUrl: "@public/images/two.png",
-    },
-    {
-      id: 3,
-      title: "Destinations you want to visit",
-      meta: "19 May 2021, 3 comments",
-      copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
-      img: three,
-      imgAlt: "@public/images/three.png",
-    },
-    {
-      id: 4,
-      title: "A letter to my family",
-      meta: "19 May 2021, 3 comments",
-      copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
-      img: four,
-      imgAlt: "@public/images/four.jpg",
-    },
-  ];
-}
+const data = [
+  {
+    id: 1,
+    title: "How to be more productive?",
+    meta: "19 May 2021, 3 comments",
+    copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
+    img: one,
+    imgAlt: "an img",
+    imgUrl: "@public/images/one.png",
+  },
+  {
+    id: 2,
+    title:
+      "Do these every single day and you will become a more creative person",
+    meta: "19 May 2021, 3 comments",
+    copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad ...",
+    img: two,
+    imgAlt: "an img",
+    imgUrl: "@public/images/two.png",
+  },
+  {
+    id: 3,
+    title: "Destinations you want to visit",
+    meta: "19 May 2021, 3 comments",
+    copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
+    img: three,
+    imgAlt: "@public/images/three.png",
+  },
+  {
+    id: 4,
+    title: "A letter to my family",
+    meta: "19 May 2021, 3 comments",
+    copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat ..",
+    img: four,
+    imgAlt: "@public/images/four.jpg",
+  },
+];
+
+export default data;
