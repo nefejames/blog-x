d91fbd1d88b2e68b4f59d9b68ebabe9774389c0b
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-20 17:35:45 -0700

    stuff

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,15 +3,15 @@ import Link from "next/link";
 import { Box, Text, Heading, Stack } from "@chakra-ui/react";
 
 /**
- * ! use either 400px or 100% as width of the img box - for my experiments
+ * ! use either 400px 800px or 100% as width of the img box - for my experiments
  *
  *
  */
 
-export default function MainCard({ title, copy, img, imgAlt, meta }) {
+export default function MainCard({ id, title, copy, img, imgAlt, meta }) {
   return (
     <Stack direction={["column", "column", "row"]} spacing={3}>
-      <Box position="relative" width={["100%", "100%", "400px"]} height="300px">
+      <Box position="relative" width={["100%", null, "800px"]} height="300px">
         <Image
           src={img}
           alt={imgAlt}
@@ -22,7 +22,7 @@ export default function MainCard({ title, copy, img, imgAlt, meta }) {
       </Box>
       <Box>
         <Box>
-          <Link href="#" passHref>
+          <Link href={`/posts/${id}`} passHref>
             <Heading as="a" size="md" fontSize={["2xl", "3xl"]}>
               {title}
             </Heading>
@@ -30,7 +30,7 @@ export default function MainCard({ title, copy, img, imgAlt, meta }) {
           <Text color="#888">{meta}</Text>
         </Box>
 
-        <Text fontFamily="Open Sans" mt={4}>
+        <Text fontFamily="Open Sans" mt={4} lineHeight="7">
           {copy}
         </Text>
       </Box>
